2bd2178cbbfb3302ee07e6ce10cf9616841790ed
Jeremy Shaw <jeremy@seereason.com>
2012-11-24 20:08:20 +0000

    remove themeDir from ClckState.

--- BootstrapTheme.hs
+++ BootstrapTheme.hs
@@ -7,11 +7,13 @@ import Clckwrks.Monad
 import Data.Text (Text)
 import Happstack.Server
 import HSP
+import Paths_clckwrks_theme_bootstrap (getDataDir)
 
 theme :: Theme
 theme = Theme
     { themeName      = "bootstrap-theme"
     , _themeTemplate = pageTemplate
+    , themeDataDir   = getDataDir
     }
 
 pageTemplate :: ( EmbedAsChild (ClckT ClckURL (ServerPartT IO)) headers
@@ -27,6 +29,7 @@ pageTemplate ttl hdr bdy =
       <title><% ttl %></title>
       <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/css/bootstrap.min.css"        rel="stylesheet" media="screen" />
       <link href="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.1.1/css/bootstrap-responsive.css" rel="stylesheet" />
+      <link rel="stylesheet" type="text/css" href=(ThemeData "style.css") />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <% hdr %>
       <% googleAnalytics %>
